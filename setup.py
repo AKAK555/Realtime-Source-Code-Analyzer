1db1268ea69d38603270c866038af1d3f0e3744b
AKAK555 <gobusyakshay@gmail.com>
2026-06-02 14:38:59 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(    name='realtime_source_code_analyzer',
+    version='0.1.0',
+    author='AK',
+    author_email='gobusyakdhay@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
